5412cb312cf994e4944e36c049992f289bcc906a
jpm620 <jpm620@stern.nyu.edu>
2015-12-21 14:08:51 -0500

    20151221 14:08
    Attempting to get ROC metrics working. Additional insight needed.

--- HFModel.py
+++ HFModel.py
@@ -3,38 +3,58 @@ Created on Dec 5, 2015
 
 @author: Joe
 '''
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
-from sklearn.naive_bayes import BernoulliNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cluster import KMeans
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+import matplotlib as plt
 import timeit
+import time
+from matplotlib.pyplot import savefig
 
 #high frequency model to predict appliance based on HF field
 def model(freqData, target):
     start = timeit.default_timer()
-    neighbors = 5
+    neighbors = 15
     model = KNeighborsClassifier(neighbors)
+    f = open('data/H3/Results/Runtime Results.txt', 'a')
+    f.write("\n\nHouse 3 Results -- Program start at "+time.strftime("%Y-%M-%D %H:%M:%S"))
     
     X_train, X_test, Y_train, Y_test = train_test_split(freqData, target, train_size=.80)
     split = timeit.default_timer()
-    print("Split in ", split-start)
+    f.write("\nSplit in "+ str(split-start)+" seconds.")
     
     model.fit(X_train, Y_train) 
     fit = timeit.default_timer()
-    print("Fit in", fit-split)
+    f.write("\nFit in "+ str(fit-split)+" seconds.")
     
     prediction = model.predict(X_test)
+    Y_test_probability = model.predict_proba(X_test)
     predict = timeit.default_timer()
-    print("Predict in", predict-fit)
+    f.write("\nPredict in "+ str(predict-fit)+" seconds.")
     
-    print("Accuracy on test = %.5f" % metrics.accuracy_score(prediction, Y_test))
-    print("Hamming loss on test = %.5f" % metrics.hamming_loss(prediction, Y_test))
+    f.write("\nAccuracy on test = %.5f" % metrics.accuracy_score(prediction, Y_test))
+    f.write("\nHamming loss on test = %.5f" % metrics.hamming_loss(prediction, Y_test))
 
-    stop = timeit.default_timer()
-    print("Total runtime:",stop-start,"seconds.")
+    # Use the metrics.roc_curve function to get the true positive rate (tpr) and false positive rate (fpr)
+    print(Y_test)
+    print (Y_test_probability)
+    fpr, tpr, thresholds = metrics.roc_curve(Y_test, Y_test_probability)
     
-    #return model, prediction, probabilities
+    # Get the area under the curve (AUC)
+    acc = np.mean(cross_validation.cross_val_score(model, freqData, target, scoring="accuracy"))
+    auc = np.mean(cross_validation.cross_val_score(model, freqData, target, scoring="roc_auc"))
+    
+        # Plot the ROC curve
+    plt.plot(fpr, tpr, label="KNN (AUC = " + str(round(auc, 2)) + ")")
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.plot([0,1], [0,1], 'k--', label="Random")
+    plt.title('Receiver Operating Characteristics')
+    plt.legend(loc=2)
+    savefig("IDS "+time.strftime("%Y-%M-%D %H:%M:%S")+".png", bbox_inches='tight')
+    
+    stop = timeit.default_timer()
+    f.write("\nTotal runtime: "+str(stop-start)+" seconds.")
+    print("Routine complete.")
+    
+    # output is now written to a file
--- main.py
+++ main.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     H3 = ld.loadData('data/H3/Tagged_Training_07_30_1343631601.mat')
     #print(H3.HF.head(5))
     #print(H3.L1.head())
-    #ld.getApplianceData(H3.HF, H3.tagInfo).to_csv('data/H3/appData.csv')
+    #ld.getApplianceData(H3.HF, H3.tagInfo).to_csv('data/H3/Results/appData.csv')
     
-    X = H3.HF
-    Y = ld.getApplianceData(X, H3.tagInfo)
-    #Y = Y.drop('Back Porch Lights 1', axis=1)
+    X = H3.HF[0:50]
+    Y = ld.getApplianceData(X, H3.tagInfo).head(50)
+    #Y = Y.drop('Bedroom Lights 1', axis=1)
     
     hf.model(X,Y)
